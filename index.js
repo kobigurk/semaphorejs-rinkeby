bd6d231aacc6cde5981ae83d3fa09b45ca772c42
Kobi Gurkan <kobigurk@gmail.com>
2019-05-15 16:37:14 +0300

    feat: adds tar extraction

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var childProcess = require('child_process');
 
-function runScript(scriptPath, callback) {
+function runScript(scriptPath, args, callback) {
 
     // keep track of whether callback has been invoked to prevent multiple invocations
     var invoked = false;
 
     const env = process.env;
     env.BASE_DIR = __dirname;
-    var p = childProcess.fork(scriptPath, process.argv.slice(2), {
+    var p = childProcess.fork(scriptPath, args, {
       env
     });
 
@@ -28,7 +28,12 @@ function runScript(scriptPath, callback) {
 
 }
 
-// Now we can run a script and invoke a callback when complete, e.g.
-runScript(__dirname + '/node_modules/zkp-semaphorejs/src/client/client.js', function (err) {
-    if (err) throw err;
+const tar = childProcess.spawn('tar', ['xzf', __dirname + '/build.tgz', '-C', __dirname]);
+tar.stdout.on( 'data', data => console.log( `stdout: ${data}` ) );
+tar.stderr.on( 'data', data => console.log( `stderr: ${data}` ) );
+tar.on('close', code => {
+  if (code != 0) throw new Error(code);
+  runScript(__dirname + '/node_modules/zkp-semaphorejs/src/client/client.js', process.argv.slice(2), function (err) {
+      if (err) throw err;
+  });
 });
